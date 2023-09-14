f6a0921a755feb0e85a850ff2761341490cd97d2
Lbahena <crazylucas_99@hotmail.com>
2023-09-13 19:28:29 -0500

    Nueva base de datos del proyecto Easy Facture

--- db/database.sql
+++ db/database.sql
@@ -1,31 +1,94 @@
-CREATE DATABASE IF NOR EXISTS Easy Facture;
+CREATE DATABASE IF NOR EXISTS Easy_Facture;
 
 
-CREATE TABLE cliente (
-    id INT(10) NOT NULL AUTO_INCREMENT,
-    id_codigo INT (10) NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(30) DEFAULT NULL,
-    apellido VARCHAR(30) DEFAULT NULL,
-    cedula INT (15) DEFAULT NULL,
-    direccion VARCHAR (60) DEFAULT NULL,
-    celular INT (15) DEFAULT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE Clientes (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(255),
+    Direccion VARCHAR(255),
+    CorreoElectronico VARCHAR(255),
+    Telefono VARCHAR(15)
 );
 
-CREATE TABLE productos (
-    id INT(10) NOT NULL AUTO_INCREMENT,
-    id_codigo INT (15) NOT NULL AUTO_INCREMENT,
-    nom_product VARCHAR (45) DEFAULT NULL,
-    precio_product INT (10) DEFAULT NULL,
-    cantidad INT (15) DEFAULT NULL,
-    PRIMARY KEY ()
+CREATE TABLE Productos (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Descripcion VARCHAR(255),
+    PrecioUnitario DECIMAL(10, 2),
+    CodigoProducto VARCHAR(50),
+    Categoria VARCHAR(50)
 );
 
-CREATE TABLE responsable (
-    id_identificacion INT (15) NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR (45) DEFAULT NULL,
-    apellido INT (10) DEFAULT NULL,
-    celular INT (15) DEFAULT NULL,
-    PRIMARY KEY ()
+CREATE TABLE Facturas (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NumeroFactura VARCHAR(20),
+    FechaEmision DATE,
+    FechaVencimiento DATE,
+    Estado VARCHAR(50)
 );
 
+CREATE TABLE DetalleFactura (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    FacturaID INT,
+    ProductoID INT,
+    Cantidad INT,
+    PrecioUnitario DECIMAL(10, 2),
+    Subtotal DECIMAL(10, 2)
+);
+
+CREATE TABLE Impuestos (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NombreImpuesto VARCHAR(50),
+    TasaImpuesto DECIMAL(5, 2),
+    MontoImpuesto DECIMAL(10, 2)
+);
+
+CREATE TABLE FormasPago (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NombreFormaPago VARCHAR(50)
+);
+
+CREATE TABLE Usuarios (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NombreUsuario VARCHAR(50),
+    CorreoElectronico VARCHAR(255),
+    Contrasena VARCHAR(255),
+    NivelAcceso INT
+);
+
+CREATE TABLE ConfiguracionEmpresa (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NombreEmpresa VARCHAR(255),
+    DireccionEmpresa VARCHAR(255),
+    ContactoEmpresa VARCHAR(255),
+    InformacionImpuestos VARCHAR(255)
+);
+
+CREATE TABLE MetodosEnvio (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NombreMetodoEnvio VARCHAR(50)
+);
+
+CREATE TABLE Auditoria (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Accion VARCHAR(255),
+    FechaAccion TIMESTAMP,
+    UsuarioID INT
+);
+
+CREATE TABLE PlantillasFactura (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NombrePlantilla VARCHAR(255),
+    ContenidoPlantilla TEXT
+);
+
+CREATE TABLE HistoricoFacturacion (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NumeroFactura VARCHAR(20),
+    FechaEmision DATE,
+    ClienteID INT,
+    ProductoID INT,
+    Cantidad INT,
+    PrecioUnitario DECIMAL(10, 2),
+    Total DECIMAL(10, 2),
+    Estado VARCHAR(50),
+    FechaEntrega DATE
+);
